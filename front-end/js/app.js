6584c92b922a87582b6ad074381c63f6a4dc3474
Adam-Greenfield <ajamesgreenfield@gmail.com>
2015-12-12 14:51:25 +0000

    added app.js

---
+++ front-end/js/app.js
@@ -0,0 +1,7 @@
+angular
+  .module('userApp', ['angular-jwt', 'ngResource', 'ui.router'])
+  .constant('API', 'http://localhost:3000/api')
+  .config(function($httpProvider){
+    $httpProvider.interceptors.push('AuthInterceptor');
+  })
+
