09e95251613e30712d0f2fd4465d283eb3d0173c
Adam-Greenfield <ajamesgreenfield@gmail.com>
2015-12-13 15:22:01 +0000

    fixed problem withput regex

--- front-end/js/controllers/championsController.js
+++ front-end/js/controllers/championsController.js
@@ -7,14 +7,10 @@ function championsController(Champion){
   var self = this
   self.champions = championJson
 
-  self.check = function(){
-    console.log('champion controller active')
-    console.log(self.champions)
-  }
 
-  self.stopGap = function(name){
-    console.log(name);
-    name = name.replace(/\s+/g, '');
-    return name;
-  }
+  // self.stopGap = function(name){
+  //   console.log(name);
+  //   name = name.replace(/\s+/g, '');
+  //   return name;
+  // }
 }
