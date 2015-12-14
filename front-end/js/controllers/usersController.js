f97d8bd038c0020440ffbdc7eb9947a2df7ac41d
Adam-Greenfield <ajamesgreenfield@gmail.com>
2015-12-14 13:04:39 +0000

    added users controller

--- front-end/js/controllers/usersController.js
+++ front-end/js/controllers/usersController.js
@@ -6,4 +6,37 @@ usersController.$inject = ['User']
 function usersController(User){
   var self = this
 
+  self.all = [];
+  self.user = {};
+
+  function userLogIn(res){
+    var token = res.token ? res.token : null
+    if(token){
+      console.log(res);
+      self.getUsers();
+      self.user = TokenService.getUser();
+      $location.path('/');
+    }
+  }
+  self.login = function(){
+    console.log('login')
+    User.login(self.user, userLogIn);
+  }
+  self.register = function() {
+    User.register(self.user, userLogIn);
+  }
+  self.logOut = function(){
+    TokenService.removeToken();
+    self.all = [];
+    self.user = {};
+  }
+  self.isLoggedIn = function(){
+    return !!TokenService.getToken();
+
+  }
+  if(self.isLoggedIn()){
+    self.getUsers();
+    self.user = TokenService.getUser();
+    console.log(self.user);
+  }
 }
---
+++ front-end/js/models/user.js
@@ -0,0 +1,18 @@
+angular
+  .module('championAPP')
+  .factory('User', User)
+
+User.$inject = ['$resource', 'API'];
+function User($resource, API){
+
+  return $resource(API + '/users:id', null, {
+    'login': { 
+      method: "POST", 
+      url: API + "/login" 
+    },
+    'register': { 
+      method: "POST", 
+      url: API + "/register"
+    },
+  });
+}
