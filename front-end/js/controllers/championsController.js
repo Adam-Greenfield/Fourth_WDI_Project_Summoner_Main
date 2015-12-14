2ceba888a867c86855e8911fee101effe7cbe712
Adam-Greenfield <ajamesgreenfield@gmail.com>
2015-12-14 09:24:36 +0000

    can send champions to the champion page

--- front-end/js/controllers/championsController.js
+++ front-end/js/controllers/championsController.js
@@ -2,11 +2,20 @@ angular
   .module('championApp')
   .controller('championsController', championsController)
 
-championsController.$inject = ['Champion']
-function championsController(Champion){
+championsController.$inject = ['$stateParams', 'Champion']
+function championsController($stateParams, Champion){
   var self = this
   self.champions = championJson
+  self.champion = {}
 
+  self.setChampion = function(champion){
+    self.champion = champion
+    console.log(self.champion);
+  }
+
+  self.showChampion = function(){
+    console.log()
+  }
 
   // self.stopGap = function(name){
   //   console.log(name);
--- front-end/js/controllers/mainRouter.js
+++ front-end/js/controllers/mainRouter.js
@@ -4,26 +4,14 @@ angular
 
 function MainRouter($stateProvider, $urlRouterProvider, $locationProvider){
   $stateProvider
-  // .state('home', {
-  //   url: "/",
-  //   templateUrl: "/partials/home.html"
-  // })
-  // .state('login', {
-  //   url: "/login",
-  //   templateUrl: "/partials/login.html"
-  // })
-  // .state('new', {
-  //   url: "/new",
-  //   templateUrl: "/partials/new.html"
-  // })
   .state('profile', {
     url: "/profile",
     templateUrl: "/partials/profile.html"
   })
-  // .state('champion', {
-  //   url: "/champion",
-  //   templateUrl: "/partials/champion.html"
-  // })
+  .state('champion', {
+    url: "/champion/:id",
+    templateUrl: "/partials/champion.html", controller: "championsController as champions"
+  })
 
   $urlRouterProvider.otherwise('/');
   $locationProvider.html5Mode({
