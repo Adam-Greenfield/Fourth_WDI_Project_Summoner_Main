f25095c291391567c6b01eab752ecf61dd7c6c97
Adam-Greenfield <ajamesgreenfield@gmail.com>
2015-12-15 10:18:05 +0000

    added skill algorythm

--- front-end/js/controllers/summonersController.js
+++ front-end/js/controllers/summonersController.js
@@ -7,7 +7,7 @@ function summonersController(Summoner, TokenService){
   var self = this
   self.user = ""
   self.name = ""
-  self.season = "SEASON2015"
+  self.season = ""
   self.champArray = []
   self.region = ""
 
@@ -33,5 +33,21 @@ function summonersController(Summoner, TokenService){
     self.champArray.forEach(function(champion){
       self.skillLevel(champion);
     })
+  }
+  self.skillLevel = function(champion){
+    console.log(champion.stats);
+    var stats = champion.stats;
+    var percentWon = (stats.totalSessionsWon) / (stats.totalSessionsPlayed) * 100;
+    var killParticipation = parseInt(((stats.totalAssists) + (stats.totalChampionKills)) /
+     ((stats.totalDeathsPerSession) + (stats.totalChampionKills) + (stats.totalAssists))
+     * 100);
+    var pentaCheck = stats.totalPentaKills;
+    var skillPercent = (percentWon + killParticipation) / 2
+
+    champion.pentaCheck = pentaCheck;
+    champion.percentWon = percentWon;
+    champion.killParticipation = killParticipation;
+    champion.skillPercent = skillPercent;
+    return champion;
   }
 }
